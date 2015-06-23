666a8c8914e6cab274641c7b8d3876b2af5a0f40
morganthomas <morgan.a.s.thomas@gmail.com>
2015-06-23 14:11:48 -0600

    Refactored JS6.

--- week-2-js/js-6/index.js
+++ week-2-js/js-6/index.js
@@ -1,28 +1,43 @@
-$(document).ready(function() {
-  var formIsDisplayed = false;
+function updateProfileFromForm() {
+  $(".input").each(function(input) {
+    $('#display-' + $(this).prop('name')).text($(this).val());
+  })
+}
 
-  $(".show-form-button").on('click', function() {
-    if (formIsDisplayed) {
-      $(".profile-edit").css("display", "none");
-      $(this).val("Show Form");
-      formIsDisplayed = false;
-    } else {
-      $(".profile-edit").css("display", "block");
-      $(this).val("Hide Form");
-
-      $(".input").each(function(input) {
-        $(this).val($('#display-' + $(this).prop('name')).text());
-      });
-
-      formIsDisplayed = true;
-    }
+function updateFormFromProfile() {
+  $(".input").each(function(input) {
+    $(this).val($('#display-' + $(this).prop('name')).text());
   });
+}
+
+function formIsDisplayed() {
+  return $(".profile-edit").css("display") === "block";
+}
+
+function hideForm() {
+  $(".profile-edit").css("display", "none");
+  $(".show-form-button").val("Show Form");
+}
+
+function showForm() {
+  $(".profile-edit").css("display", "block");
+  $(".show-form-button").val("Hide Form");
+  updateFormFromProfile();
+}
+
+function showFormClickHandler() {
+  if (formIsDisplayed()) {
+    hideForm();
+  } else {
+    showForm();
+  }
+}
+
+$(document).ready(function() {
+  $(".show-form-button").on('click', showFormClickHandler);
 
   $('.submit').on('click', function(event) {
     event.preventDefault();
-
-    $(".input").each(function(input) {
-      $('#display-' + $(this).prop('name')).text($(this).val());
-    })
+    updateProfileFromForm();
   });
 });
