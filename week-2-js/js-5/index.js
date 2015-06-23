3a00e8d9c9d46116bd0a7bf60bf0e00b69e9642b
morganthomas <morgan.a.s.thomas@gmail.com>
2015-06-23 11:13:02 -0600

    JS5 bonus.

--- week-2-js/js-5/index.js
+++ week-2-js/js-5/index.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
-  var profileItemBeingEdited = null;
+  var itemBeingEdited = null;
   var inPlaceEditor = null;
 
-  $(".profile").on("click", "dd", function() {
-    profileItemBeingEdited = $(this);
-    profileItemBeingEdited.css("visibility", "hidden");
-    var pos = profileItemBeingEdited.position();
+  $("body").on("click", ".editable", function() {
+    itemBeingEdited = $(this);
+    itemBeingEdited.css("visibility", "hidden");
+    var pos = itemBeingEdited.position();
 
     inPlaceEditor = $('<input type="text" class="in-place-editor">');
     inPlaceEditor.css("top", pos.top);
     inPlaceEditor.css("left", pos.left);
-    inPlaceEditor.val(profileItemBeingEdited.text());
+    inPlaceEditor.val(itemBeingEdited.text());
 
-    $(".profile").append(inPlaceEditor);
+    itemBeingEdited.offsetParent().append(inPlaceEditor);
     inPlaceEditor.focus();
   });
 
-  $(".profile").on("blur", ".in-place-editor", function() {
+  $("body").on("blur", ".in-place-editor", function() {
     // This works because there is only one in place editor at a time, so
     // we can assume the one that triggered this event is the one we have
     // stored in our variables.
-    profileItemBeingEdited.css("visibility", "visible");
-    profileItemBeingEdited.text(inPlaceEditor.val());
+    itemBeingEdited.css("visibility", "visible");
+    itemBeingEdited.text(inPlaceEditor.val());
     inPlaceEditor.remove();
-    profileItemBeingEdited = null;
+    itemBeingEdited = null;
     inPlaceEditor = null;
   });
 });
